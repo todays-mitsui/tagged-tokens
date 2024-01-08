c9328a2decb10070abfcf89a04a99f2626eb7914
todays_mitsui <todays_mitsui@hatena.ne.jp>
2024-01-08 09:41:52 +0900

    Rust 実装を消した

--- rust/src/tag.rs
+++
@@ -1,7 +0,0 @@
-mod get_range;
-mod tag;
-
-pub use get_range::get_range;
-
-#[derive(Clone, Debug, PartialEq)]
-pub struct Tag(Vec<usize>);
--- rust/src/token.rs
+++
@@ -1,5 +0,0 @@
-mod token;
-mod tokenize;
-
-pub use token::Token;
-pub use tokenize::tokenize;
